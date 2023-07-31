ccd7b46db1cfd5d6a92eb69d01513aaf44acb2d5
selcannarin <narinselcan@gmail.com>
2023-07-31 12:48:43 +0300

    login ui added

---
+++ app/src/main/java/com/selcannarin/schoolbustrackerdriver/ui/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.selcannarin.schoolbustrackerdriver.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.selcannarin.schoolbustrackerdriver.R
+import com.selcannarin.schoolbustrackerdriver.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.signInFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
