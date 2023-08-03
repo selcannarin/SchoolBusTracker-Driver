995a4a5d49d43d16ecc315e2071c2b09e92754cf
selcannarin <narinselcan@gmail.com>
2023-08-03 10:57:46 +0300

    added FirebaseEvents.kt

---
+++ app/src/main/java/com/selcannarin/schoolbustrackerdriver/data/remote/FirebaseEvents.kt
@@ -0,0 +1,7 @@
+package com.selcannarin.schoolbustrackerdriver.data.remote
+
+sealed class FirebaseEvents {
+    data class Message(val message: String) : FirebaseEvents()
+    data class ErrorCode(val code: Int) : FirebaseEvents()
+    data class Error(val error: String) : FirebaseEvents()
+}
