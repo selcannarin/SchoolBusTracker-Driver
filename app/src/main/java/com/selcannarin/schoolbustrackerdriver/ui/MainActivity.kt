522d0899c2045a8a2554becb62a54281bc26c9a4
selcannarin <narinselcan@gmail.com>
2023-08-03 16:01:53 +0300

    added bottom menu view

--- app/src/main/java/com/selcannarin/schoolbustrackerdriver/ui/MainActivity.kt
+++ app/src/main/java/com/selcannarin/schoolbustrackerdriver/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.selcannarin.schoolbustrackerdriver.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.selcannarin.schoolbustrackerdriver.R
 import com.selcannarin.schoolbustrackerdriver.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,5 +19,46 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.navHostFragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val visibleDestinations = setOf(
+                R.id.profileFragment,
+                R.id.attendanceFragment,
+                R.id.parentsFragment
+            )
+
+            binding.bottomNavigationView.visibility = if (destination.id in visibleDestinations) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
+        binding.bottomNavigationView.setupWithNavController(navController)
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.attendance -> {
+                    navController.navigate(R.id.attendanceFragment)
+                    true
+                }
+                R.id.parents -> {
+                    navController.navigate(R.id.parentsFragment)
+                    true
+                }
+                R.id.profile -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                else -> {
+                    navController.navigate(R.id.attendance)
+                    true
+                }
+            }
+        }
     }
 }
